17d5f36ce42991e729e6c1285b45a32c913f0a47
Alexander Wilkinson <alexwilkinson@gmail.com>
2026-05-12 21:28:00 +0100

    feat(cli loop): added in the loop to allow for many questions to be asked

--- packages/board-game-agent/src/agents/query/index.ts
+++ packages/board-game-agent/src/agents/query/index.ts
@@ -6,13 +6,34 @@ import { markedTerminal } from "marked-terminal";
 
 const TEST_QUESTION = `I want something like Catan but with more conflict`;
 
-export const main = async (userQuestion = TEST_QUESTION) => {
-  logger.info("Asking A wizard for an answer...");
-  // run the prompt extractor
+/**
+ * Function to transform the prompt to a much richer version
+ * then run the llm to get the data from database and
+ * then run a writer to turn that data into written piece
+ * which is exported via markdown
+ * @param userQuestion string
+ * @returns string markdown format
+ */
+export const askQueryReturnMarkdown = async (userQuestion: string) => {
   const dataExtracted = await runPromptTransformation(userQuestion);
 
   logger.info("Data Extracted now on to the fun part...");
   const markdown = await runGameWriter(dataExtracted, userQuestion);
+
+  return markdown;
+};
+
+/**
+ * Main function that will transform prompt, query the llm and get
+ * markdown data back
+ * will then used marked to display correctly in the terminal
+ * This is a stand alone function
+ * @param userQuestion string
+ */
+const main = async (userQuestion = TEST_QUESTION) => {
+  logger.info("Asking A wizard for an answer...");
+
+  const markdown = await askQueryReturnMarkdown(userQuestion);
 
   // display the results
   marked.use(markedTerminal());
--- packages/board-game-agent/src/cli/query/index.ts
+++ packages/board-game-agent/src/cli/query/index.ts
@@ -1,5 +1,24 @@
 import { logger } from "@repo/utils";
+import ora from "ora";
+import chalk from "chalk";
+import { marked } from "marked";
+import { markedTerminal } from "marked-terminal";
+import { askQueryReturnMarkdown } from "../../agents/query";
+
+const spinner = ora("Loading unicorns");
 
 export const handleQuery = async (prompt: string) => {
   logger.debug(`Asking query with prompt:`, prompt);
+
+  spinner.start();
+  const markdownString = await askQueryReturnMarkdown(prompt);
+
+  marked.use(markedTerminal());
+
+  const output = await marked.parse(markdownString);
+
+  spinner.stop();
+
+  console.log(chalk.magenta("Agent results:"));
+  process.stdout.write(output);
 };
