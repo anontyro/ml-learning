7aaa86edbf6c149b2bfdaf481b8a530d6e809224
Alexander Wilkinson <alexwilkinson@gmail.com>
2026-05-12 22:11:09 +0100

    feat(added message history): added message history class will hold basic information about made singleton added a basic compact and methods to view data

---
+++ packages/board-game-agent/src/utils/memory/MessageHistory.ts
@@ -0,0 +1,66 @@
+import { logger } from "@repo/utils";
+
+type DataStored = {
+  role: "user" | "assistant";
+  content: string;
+};
+
+class MessageHistory {
+  private constructor(tokenLimit = 3500) {
+    this.tokenLimit = tokenLimit;
+  }
+
+  private static instance: MessageHistory | null = null;
+  private tokenLimit: number;
+  private data: DataStored[] = [];
+
+  public static getInstance(tokenLimit?: number) {
+    if (!MessageHistory.instance) {
+      MessageHistory.instance = new MessageHistory(tokenLimit);
+    }
+
+    return MessageHistory.instance;
+  }
+
+  public push(userData: DataStored) {
+    this.data.push(userData);
+    this.compact();
+  }
+
+  public get() {
+    return [...this.data];
+  }
+
+  public clear() {
+    this.data = [];
+  }
+
+  public tokenCount() {
+    const totalContent = this.data.map((x) => x.content).join("");
+    const tokenApprox = Math.ceil(totalContent.length / 4);
+
+    return tokenApprox;
+  }
+
+  public tokenPercentageUsage() {
+    const tokens = this.tokenCount();
+
+    return (tokens / this.tokenLimit) * 100;
+  }
+
+  public seralize() {}
+
+  private compact() {
+    while (this.tokenCount() > this.tokenLimit) {
+      const oldestMessage = this.data.shift();
+
+      if (oldestMessage) {
+        logger.debug(`Compacting, removing oldest message`, oldestMessage);
+      } else {
+        break;
+      }
+    }
+  }
+}
+
+export default MessageHistory;
