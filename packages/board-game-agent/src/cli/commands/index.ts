3329a6f4a44bac822d92636527854ade4b5de04c
Alexander Wilkinson <alexwilkinson@gmail.com>
2026-05-13 22:17:30 +0100

    feat(commands): added more commadns for memory

--- packages/board-game-agent/src/cli/commands/index.ts
+++ packages/board-game-agent/src/cli/commands/index.ts
@@ -1,4 +1,35 @@
 import chalk from "chalk";
+import MessageHistory from "../../utils/memory/MessageHistory";
+
+const messageHistoryInstance = MessageHistory.getInstance();
+
+const memory = () => {
+  const memoryUsage = Math.floor(messageHistoryInstance.tokenPercentageUsage());
+  if (memoryUsage > 75) {
+    console.log(chalk.redBright(`Memory is currently at ${memoryUsage}%`));
+    console.log(chalk.redBright(`Its probably time to /compact`));
+    return;
+  }
+  if (memoryUsage > 50) {
+    console.log(chalk.yellowBright(`Memory is currently at ${memoryUsage}%`));
+    console.log(chalk.yellowBright(`You may want to consider compacting soon`));
+    return;
+  }
+  console.log(chalk.greenBright(`Memory is currently at ${memoryUsage}%`));
+  console.log(chalk.greenBright(`Still plenty of head room`));
+};
+
+const clear = () => {
+  console.log(chalk.redBright("purging memory"));
+  messageHistoryInstance.clear();
+  console.log(chalk.redBright("completed"));
+};
+
+const compact = () => {
+  console.log(chalk.redBright("clearing old data"));
+  messageHistoryInstance.compact();
+  console.log(chalk.redBright("completed"));
+};
 
 const quit = () => {
   console.log(chalk.redBright("Until next time..."));
@@ -15,4 +46,7 @@ const help = () => {
 export const COMMANDS: Record<string, () => void> = {
   "/quit": quit,
   "/help": help,
+  "/memory": memory,
+  "/clear": clear,
+  "/compact": compact,
 };
